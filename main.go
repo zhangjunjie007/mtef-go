8d2dc2b8fe2c0238c0c03663c2187d2320ec11bf
zhangjunjie007 <17310319450@163.com>
2023-07-10 15:17:26 +0800

    go解析mathtype公式

--- main.go
+++ main.go
@@ -2,81 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/urfave/cli"
-	"github.com/zhexiao/mtef-go/docx"
 	"github.com/zhexiao/mtef-go/eqn"
-	"log"
-	"os"
-	"time"
+	"net/http"
 )
 
 func main() {
-	var filepath, docxDocument string
-
-	app := cli.NewApp()
-	app.Name = "Mtef"
-	app.Usage = "Convert MSDocx Mathtype Ole object to Latex code"
-	app.Version = "2.0"
-	app.EnableBashCompletion = true
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:        "filepath, f",
-			Usage:       "Mathtype Ole object filepath",
-			Destination: &filepath,
-		},
-		cli.StringFlag{
-			Name:        "wordDocx, w",
-			Usage:       "Office word docx documents",
-			Destination: &docxDocument,
-		},
-	}
-
-	app.Action = func(c *cli.Context) error {
-		if filepath != "" {
-			if _, err := os.Stat(filepath); os.IsNotExist(err) {
-				fmt.Println("File not exist!!!!")
-				return nil
-			}
-
-			//转换数据
-			latex := eqn.Convert(filepath)
-			fmt.Println(latex)
-			return nil
-		}
-
-		if docxDocument != "" {
-			if _, err := os.Stat(docxDocument); os.IsNotExist(err) {
-				fmt.Println("File not exist!!!!")
-				return nil
-			}
-
-			dw := docx.DocxWord{
-				Filename: docxDocument,
-				Target:   fmt.Sprintf("/tmp/%v", time.Now().UnixNano()),
-			}
-
-			//转换数据
-			err := dw.ParseDocx()
-			if err != nil {
-				return err
-			}
-		}
-
-		return nil
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	//转换数据,测试开发使用，需要注释上面的所有代码
-	//startEqn := 1
-	//endEqn := 2
-	//for i := startEqn; i <= endEqn; i++ {
-	//	pathName := fmt.Sprintf("F:/workspace/goproj/src/mtef-go/test/oleObject%v.bin", i)
-	//	latex := eqn.Convert(pathName)
-	//	fmt.Println("num:", i, "latex:", latex)
-	//}
+	http.HandleFunc("/", ServeHTTP)
+	http.ListenAndServe(":33333", nil)
+}
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//路径中获取参数
+	paraName := r.URL.Path[1:]
+	// latex := eqn.Convert("E:/go_env/works/src/mtef-go-master/test/oleObject1.bin")
+	latex := eqn.Convert(paraName)
+	fmt.Println(paraName)
+	fmt.Fprintf(w, latex)
 }
